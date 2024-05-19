34640cd8cecc1829acf2428cae567d5c3166182d
Kostya <prol4588@gmail.com>
2024-05-19 22:12:26 +0700

    fix

--- src/shared/api/providerApi.ts
+++ src/shared/api/providerApi.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_API_URL = 'http://127.0.0.1:3210/api';
+// const BASE_API_URL = 'http://127.0.0.1:3210/api';
+const BASE_API_URL = 'http://151.248.115.92:3210/api';
 
 export const providerApi = createApi({
   reducerPath: 'providerApi',
